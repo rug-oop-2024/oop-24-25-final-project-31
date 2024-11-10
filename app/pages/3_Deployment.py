1e37e2c2984c17ec23bc8f2d126e1df38712a9c4
Arkhan12345 <arkhan.aspi05@gmail.com>
2024-11-10 22:14:28 +0100

    updated Deployment.py

--- app/pages/3_Deployment.py
+++ app/pages/3_Deployment.py
@@ -12,21 +12,31 @@ def write_helper_text(text: str):
 st.write("Deployment")
 write_helper_text("In this section, you can see saved pipelines.")
 
-automl = AutoMLSystem.get_instance()
 
-saved_pipe = automl.registry.list(type="pipeline")
+class DeploymentPage:
+    def __init__(self):
+        """Initialize the AutoML system and retrieve saved pipelines."""
+        self.automl = AutoMLSystem.get_instance()
+        self.saved_pipe = self.automl.registry.list(type="pipeline")
 
-if saved_pipe:
-    st.write(f"Found {len(saved_pipe)} saved pipelines.")
+    def run(self):
+        if self.saved_pipe:
+            st.write(f"Found {len(self.saved_pipe)} saved pipelines.")
 
-if saved_pipe:
-    for pipeline in saved_pipe:
-        pipeline_data = pickle.loads(pipeline.data)
-        st.write(f"Pipeline Name: {pipeline.name}")
-        st.write(f"Version: {pipeline.version}")
-        st.write(f"Model: {pipeline_data.get('model')}")
-        st.write(f"Input Features: \
-                 {', '.join(pipeline_data.get('input_features'))}")
-        st.write(f"Target Feature: {pipeline_data.get('target_feature')}")
-        st.write(f"Split Ratio: {pipeline_data.get('split_ratio')}")
-        st.write(f"Metrics: {', '.join(pipeline_data.get('metrics'))}")
+        if self.saved_pipe:
+            for pipeline in self.saved_pipe:
+                pipeline_data = pickle.loads(pipeline.data)
+                st.write(f"Pipeline Name: {pipeline.name}")
+                st.write(f"Version: {pipeline.version}")
+                st.write(f"Model: {pipeline_data.get('model')}")
+                st.write(f"Input Features: \
+                        {', '.join(pipeline_data.get('input_features'))}")
+                st.write(f"Target Feature: \
+                         {pipeline_data.get('target_feature')}")
+                st.write(f"Split Ratio: {pipeline_data.get('split_ratio')}")
+                st.write(f"Metrics: {', '.join(pipeline_data.get('metrics'))}")
+
+
+if __name__ == "__main__":
+    page = DeploymentPage()
+    page.run()
