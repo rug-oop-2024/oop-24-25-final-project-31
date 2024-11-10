bfb5d067fb8accca98e29153969f03b603776a32
Arkhan12345 <arkhan.aspi05@gmail.com>
2024-11-10 19:55:31 +0100

    finished Datasets and Modelling(importing datasets into modelling is not possible so Modelling will continue to load

--- app/core/system.py
+++ app/core/system.py
@@ -1,20 +1,34 @@
 from autoop.core.storage import LocalStorage
 from autoop.core.database import Database
-from autoop.core.ml.dataset import Dataset
 from autoop.core.ml.artifact import Artifact
 from autoop.core.storage import Storage
 from typing import List
 
 
 class ArtifactRegistry():
-    def __init__(self, 
+    def __init__(self,
                  database: Database,
                  storage: Storage):
+        """
+        Initialize the ArtifactRegistry with a database and storage.
+
+        Args:
+            database (Database): The database instance to store
+            artifact metadata.
+            storage (Storage): The storage instance to manage artifact data.
+        """
         self._database = database
         self._storage = storage
 
     def register(self, artifact: Artifact):
         # save the artifact in the storage
+        """
+        Registers an artifact in the database and storage.
+
+        Args:
+            artifact (Artifact): The artifact to register.
+
+        """
         self._storage.save(artifact.data, artifact.asset_path)
         # save the metadata in the database
         entry = {
@@ -25,9 +39,18 @@ class ArtifactRegistry():
             "metadata": artifact.metadata,
             "type": artifact.type,
         }
-        self._database.set(f"artifacts", artifact.id, entry)
-    
-    def list(self, type: str=None) -> List[Artifact]:
+        self._database.set("artifacts", artifact.id, entry)
+
+    def list(self, type: str = None) -> List[Artifact]:
+        """
+        Lists all artifacts in the registry.
+
+        Args:
+            type (str, optional): Filter artifacts by type. Defaults to None.
+
+        Returns:
+            List[Artifact]: A list of artifacts in the registry.
+        """
         entries = self._database.list("artifacts")
         artifacts = []
         for id, data in entries:
@@ -44,8 +67,17 @@ class ArtifactRegistry():
             )
             artifacts.append(artifact)
         return artifacts
-    
+
     def get(self, artifact_id: str) -> Artifact:
+        """
+        Retrieves an artifact from the registry.
+
+        Args:
+            artifact_id (str): The id of the artifact.
+
+        Returns:
+            Artifact: The artifact with the given id.
+        """
         data = self._database.get("artifacts", artifact_id)
         return Artifact(
             name=data["name"],
@@ -56,33 +88,62 @@ class ArtifactRegistry():
             data=self._storage.load(data["asset_path"]),
             type=data["type"],
         )
-    
+
     def delete(self, artifact_id: str):
+        """
+        Deletes an artifact from the registry.
+
+        This method removes the artifact's data from the storage and
+        deletes its metadata from the database.
+
+        Args:
+            artifact_id (str): The id of the artifact to delete.
+        """
         data = self._database.get("artifacts", artifact_id)
         self._storage.delete(data["asset_path"])
         self._database.delete("artifacts", artifact_id)
-    
+
 
 class AutoMLSystem:
     _instance = None
 
     def __init__(self, storage: LocalStorage, database: Database):
+        """
+        Initializes an instance of the AutoMLSystem class.
+
+        The AutoMLSystem is a singleton class that manages the storage and
+        database of the AutoML system.
+
+        Args:
+            storage (LocalStorage): The local storage instance.
+            database (Database): The database instance.
+        """
         self._storage = storage
         self._database = database
         self._registry = ArtifactRegistry(database, storage)
 
     @staticmethod
     def get_instance():
+        """
+        Gets the instance of the AutoMLSystem singleton class.
+
+        The first time this method is called, it creates an instance of the
+        AutoMLSystem class and initializes its storage and database.
+        Subsequent calls will return the same instance.
+
+        Returns:
+            AutoMLSystem: The instance of the AutoMLSystem class.
+        """
         if AutoMLSystem._instance is None:
             AutoMLSystem._instance = AutoMLSystem(
-                LocalStorage("./assets/objects"), 
+                LocalStorage("./assets/objects"),
                 Database(
                     LocalStorage("./assets/dbo")
                 )
             )
         AutoMLSystem._instance._database.refresh()
         return AutoMLSystem._instance
-    
+
     @property
     def registry(self):
         return self._registry
--- app/pages/2_⚙_Modelling.py
+++ app/pages/2_⚙_Modelling.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from typing import Optional, List, Tuple, Union
 
 from autoop.core.ml.feature import Feature
 from autoop.functional.feature import detect_feature_types
@@ -20,7 +21,6 @@ from autoop.core.ml.model.classification.support_vector import (
 )
 from autoop.core.ml.model.classification.decision_tree import DTreeClassifier
 
-
 from autoop.core.ml.metric import get_metric, METRICS
 from app.core.system import AutoMLSystem
 from autoop.core.ml.dataset import Dataset
@@ -37,134 +37,241 @@ st.write("# ⚙ Modelling")
 write_helper_text("In this section, you can design a machine learning pipeline\
                   to train a model on a dataset.")
 
-automl = AutoMLSystem.get_instance()
 
-datasets = automl.registry.list(type="dataset")
+class PipelineModelling:
+    """Encapsulates the pipeline modelling."""
+    def __init__(self):
+        """Initialize AutoML system and load available datasets."""
+        self.automl = AutoMLSystem.get_instance()
+        self.datasets = self._list()
 
-# your code here
-# ST/modelling/datasets/list: Load existing datasets using the artifact
-# registry. You can use a select box to achieve this.
-st.header("Step 1: Load the Dataset")
+    def _list(self) -> List[Dataset]:
+        """
+        Loads the list of available datasets from the AutoML system registry.
 
-dataset_list = [ds.name for ds in datasets]
-selected_dataset = st.selectbox("Select a Dataset:", dataset_list)
+        Returns:
+            List[Dataset]: A list of datasets registered in the AutoML system.
+        """
+        return self.automl.registry.list(type="dataset")
 
-if selected_dataset:
-    dataset = next(ds for ds in datasets if ds.name == selected_dataset)
-    data = Dataset(
-        name=dataset.name,
-        asset_path=dataset.asset_path,
-        version=dataset.version,
-        data=dataset.data
-    )
-    df = data.read()
-    st.write(df)
+    def _select_dataset(self) -> Optional[Dataset]:
+        """
+        Select a dataset from the AutoML system registry using
+        a Streamlit selectbox.
 
-    # ST/modelling/datasets/features: Detect the features and generate a
-    # selection menu for selecting the input features (many)
-    # and one target feature.
-    # Based on the feature selections, prompt the user with the detected task
-    # type (i.e., classification or regression).
+        Returns:
+            Optional[Dataset]: A dataset selected from the AutoML system
+            registry, or None.
+        """
+        dataset_list = [ds.name for ds in self.datasets]
+        selected_name = st.selectbox("Select a Dataset:", dataset_list)
+        if selected_name:
+            dataset = next(ds for ds
+                           in self.datasets if ds.name == selected_name)
+            return Dataset(
+                name=dataset.name,
+                asset_path=dataset.asset_path,
+                version=dataset.version,
+                data=dataset.data
+            )
+        return None
 
-    st.header("Step 2: Feature Selection and task type detection")
-    features = detect_feature_types(data)
-    numerical_features = [f.name for f in features if f.type == "numerical"]
-    categorical_features = [f.name for f
-                            in features if f.type == "categorical"]
+    def _features(self, dataset: Dataset) -> (Tuple[List[Feature],
+                                                    Feature, str]):
+        """
+        Detects the features in a dataset and returns a tuple
+        containing the list of input features, the target feature,
+        and the task type.
 
-    input_features = []
-    input_feature_names = st.multiselect("Select input features",
-                                         numerical_features +
-                                         categorical_features)
-    for feature_name in input_feature_names:
-        if feature_name in numerical_features:
+        Parameters:
+            dataset (Dataset): The dataset to detect features from.
+
+        Returns:
+            Tuple[List[Feature], Feature, str]: A tuple containing the
+            list of input features, the target feature, and the task type.
+        """
+        df = dataset.read()
+        features = detect_feature_types(dataset)
+        numerical_features = [f.name for
+                              f in features if f.type == "numerical"]
+        categorical_features = [f.name for
+                                f in features if f.type == "categorical"]
+
+        input_features = []
+        input_feature_names = st.multiselect("Select input features",
+                                             numerical_features +
+                                             categorical_features)
+        for feature_name in input_feature_names:
+            if feature_name in numerical_features:
+                feature_type = "numerical"
+            elif feature_name in categorical_features:
+                feature_type = "categorical"
+            else:
+                raise ValueError(f"Unknown feature type for {feature_name}")
+            input_features.append(
+                Feature(name=feature_name, type=feature_type))
+
+        target_feature_name = st.selectbox("Select target feature:",
+                                           df.columns)
+        if target_feature_name in numerical_features:
             feature_type = "numerical"
-        elif feature_name in categorical_features:
-            feature_type = "categorical"
+            task_type = "regression"
         else:
-            raise ValueError(f"Unknown feature type for {feature_name}")
-        input_features.append(Feature(name=feature_name, type=feature_type))
+            feature_type = "categorical"
+            task_type = "classification"
+        target_feature = Feature(name=target_feature_name, type=feature_type)
 
-    target_feature_name = st.selectbox("Select target feature:", df.columns)
-    if target_feature_name in numerical_features:
-        feature_type = "numerical"
-        task_type = "regression"
-    else:
-        feature_type = "categorical"
-        task_type = "classification"
-    target_feature = Feature(name=target_feature_name, type=feature_type)
+        st.write(f"Detected Task Type: {task_type}")
+        return input_features, target_feature, task_type
 
-    st.write(f"Detected Task Type: {task_type}")
+    def models(self, task_type: str) -> Union[LassoRegression, RidgeRegression,
+                                              MultipleLinearRegression,
+                                              KNearestClassifier,
+                                              SupportVectorClassifier,
+                                              DTreeClassifier]:
+        """
+        Prompts the user to select a machine learning model.
 
-    # ST/modelling/models: Prompt the user to select a model
-    # based on the task type.
+        Args:
+            task_type (str): The type of task, which can be either
+            "regression" or "classification".
 
-    st.header("Step 3: Model selection")
+        Returns:
+            Union[LassoRegression, RidgeRegression, MultipleLinearRegression,
+                KNearestClassifier, SupportVectorClassifier, DTreeClassifier]:
+                The selected model instance corresponding to
+                the chosen task type.
+        """
+        if task_type == "regression":
+            model = st.selectbox("Select Regression Model:",
+                                 [LassoRegression(),
+                                  RidgeRegression(),
+                                  MultipleLinearRegression()])
+        else:
+            model = st.selectbox("Select Classification Model:",
+                                 [KNearestClassifier(),
+                                  SupportVectorClassifier(),
+                                  DTreeClassifier()])
+        return model
 
-    if task_type == "regression":
-        model = st.selectbox("Select Regression Model: ",
-                             [LassoRegression(),
-                              RidgeRegression(),
-                              MultipleLinearRegression()])
-    else:
-        model = st.selectbox("Select Classification Model: ",
-                             [KNearestClassifier(),
-                              SupportVectorClassifier(),
-                              DTreeClassifier()])
+    def split(self) -> float:
+        """
+        Prompts the user to select the proportion of the dataset to be used
+        for the test set.
 
-    # ST/modelling/pipeline/split: Prompt the user to select a dataset split.
+        Returns:
+            float: The proportion of the dataset to be used as the test set,
+            as selected by the user.
+        """
+        return st.slider("Test Set Proportion", 0.1, 0.5, 0.2)
 
-    st.header("Step 4: Splitting the data")
-    test_size = st.slider("Test Set Proportion", 0.1, 0.5, 0.2)
+    def metrics(self, task_type: str) -> List[str]:
+        """
+        Prompts the user to select a metric for the given task type.
 
-    # ST/modelling/pipeline/metrics: Prompt the user to select
-    # a set of compatible metrics.
+        Args:
+            task_type (str): The type of task, which can be either
+            "regression" or "classification".
 
-    st.header("Step 5: Select Metrics")
+        Returns:
+            List[str]: A list containing a single string representing the
+            selected metric.
+        """
+        if task_type == "regression":
+            selected_metric = st.selectbox("Select Metric:", METRICS[:3])
+        else:
+            selected_metric = st.selectbox("Select Metric:", METRICS[3:])
+        metric = get_metric(selected_metric)
+        return [metric]
 
-    if task_type == "regression":
-        selected_metric = st.selectbox("Select Metric: ", METRICS[:3])
-    else:
-        selected_metric = st.selectbox("Select Metric: ", METRICS[3:])
+    def summary(self, model, dataset: Dataset, input_features: List[Feature],
+                target_feature: Feature, test_size: float, metrics: List[str]):
 
-    metric = get_metric(selected_metric)
-    st.write(f"Selected Metric: {metric}")
+        """
+        Shows a summary of the pipeline and returns the pipeline object.
 
-    # ST/modelling/pipeline/summary: Prompt the user with a beautifuly
-    # formatted pipeline summary with all the configurations.
+        This function receives the model, dataset, input and target features,
+        split ratio and metrics as parameters.
+        It creates a pipeline object with the given parameters and shows a
+        summary of the pipeline if the button is clicked.
+        The summary includes the model name, input and target feature names,
+        split ratio and metric names.
+        Finally, it returns the pipeline object.
+        """
+        pipeline = Pipeline(
+                metrics=metrics,
+                dataset=dataset,
+                model=model,
+                input_features=input_features,
+                target_feature=target_feature,
+                split=test_size
+            )
 
-    st.header("Step 6: Pipeline Summary")
+        if st.button("Show Pipeline Summary"):
+            st.write("Pipeline Summary")
+            st.write("Model:", model)
+            st.write("Input Features:", [f.name for f in input_features])
+            st.write("Target Feature:", target_feature.name)
+            st.write("Split Ratio:", test_size)
+            st.write("Metrics:", [metric.name for metric in metrics])
 
-    pipeline = Pipeline(
-            metrics=[metric],
-            dataset=data,
-            model=model,
-            input_features=input_features,
-            target_feature=target_feature,
-            split=test_size
-        )
+        return pipeline
 
-    if st.button("Show Pipeline Summary"):
-        st.write("Pipeline Summary")
-        st.write("Model:", model)
-        st.write("Input Features:", [f.name for f in input_features])
-        st.write("Target Feature:", target_feature.name)
-        st.write("Split Ratio:", test_size)
-        st.write("Metrics:", metric.name)
+    def train(self, pipeline: Pipeline):
+        """
+        Train a model with the given pipeline and display the results.
 
-    # ST/modelling/pipeline/train: Train the class and report
-    # the results of the pipeline.
+        Parameters:
+        pipeline (Pipeline): the pipeline to use for training and evaluation
 
-    st.header("Step 7: Training and evaluation")
-    if st.button("Train & Evaluate Model"):
-        results = pipeline.execute()
-        st.write("Model Trained")
+        Returns:
+        None
+        """
+        if st.button("Train & Evaluate Model"):
+            results = pipeline.execute()
+            st.write("Model Trained")
 
-        st.header("Pipeline Results")
-        st.write("Test Metrics:")
-        for metric_obj, value in results["test metrics"]:
-            st.write(f"{metric_obj.name}: {value}")
+            st.header("Pipeline Results")
+            st.write("Test Metrics:")
+            for metric_obj, value in results["test metrics"]:
+                st.write(f"{metric_obj.name}: {value}")
 
-        st.write("Training Metrics:")
-        for metric_obj, value in results["training metrics"]:
-            st.write(f"{metric_obj.name}: {value}")
+            st.write("Training Metrics:")
+            for metric_obj, value in results["training metrics"]:
+                st.write(f"{metric_obj.name}: {value}")
+
+
+def main():
+    page = PipelineModelling()
+
+    st.header("Step 1: Load the Dataset")
+    dataset = page._select_dataset()
+    if dataset:
+        df = dataset.read()
+        st.write(df)
+
+        st.header("Step 2: Feature Selection and Task Type Detection")
+        input_features, target_feature, task_type = page._features(dataset)
+
+        st.header("Step 3: Model Selection")
+        model = page.models(task_type)
+
+        st.header("Step 4: Split the Data")
+        split_ratio = page.split()
+
+        st.header("Step 5: Select Metrics")
+        metrics = page.metrics(task_type)
+
+        st.header("Step 6: Pipeline Summary")
+        pipeline = page.summary(model, dataset,
+                                input_features,
+                                target_feature,
+                                split_ratio,
+                                metrics)
+
+        st.header("Step 7: Train and Evaluate Model")
+        page.train(pipeline)
+
+
+if __name__ == "__main__":
+    main()
